9c96093bab4d94645a29abe5fc52f1338ce56c26
skonry <skonry1604@gmail.com>
2021-11-08 19:28:31 +0100

    Updated ProductList component to show data from api

--- src/pages/ProductList.tsx
+++ src/pages/ProductList.tsx
@@ -1,6 +1,29 @@
+import useApi from '../hooks/useApi';
+import { ProductAjaxDto } from '../api-types';
+
 function ProductList() {
+  const { data: products = [] }: { data: ProductAjaxDto[]} = useApi('products');
+
   return (
-    <div>ProductList</div>
+    <div>
+      <h1>List of products</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => (
+            <tr key={product.id}>
+              <td>{product.name}</td>
+              <td>{product.category?.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
