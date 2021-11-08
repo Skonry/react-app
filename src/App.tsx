4755e42a50d6e3c9df95a250b19ff866a60e3782
skonry <skonry1604@gmail.com>
2021-11-08 13:20:37 +0100

    Edited App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
-function App() {
+import Navbar from './components/Navbar';
+
+type AppProps = {
+  children: React.ReactNode;
+}
+
+function App({ children }: AppProps) {
   return (
     <div>
+      <Navbar />
+      <main>{children}</main>
     </div>
   );
 }
