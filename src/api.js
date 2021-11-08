ba92b891206f1fc1ca10f03f601d40a475d28b91
skonry <skonry1604@gmail.com>
2021-11-09 00:29:20 +0100

    Updated CreateContent page to contain CategoryForm and ProductForm

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 const organizationId = 219;
 export const apiUrl = `https://newdemostock.gopos.pl/ajax/${organizationId}`;
 export const apiHeaders = {
-  'Authorization': 'fd9ba9e1-0788-4e8f-ac46-a43df43e205e'
+  'Authorization': 'fd9ba9e1-0788-4e8f-ac46-a43df43e205e',
+  'Content-Type': 'application/json'
 };
--- src/pages/CreateContent.tsx
+++ src/pages/CreateContent.tsx
@@ -1,6 +1,35 @@
+import ProductForm from '../components/ProductForm';
+import CategoryForm from '../components/CategoryForm';
+
 function CreateContent() {
   return (
-    <div>CreateContent</div>
+    <div>
+      <h1 className="text-center my-5">CreateContent</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="card">
+              <div className="card-header">
+                <h2>Create Product</h2>
+              </div>
+              <div className="card-body">
+                <ProductForm />
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="card">
+              <div className="card-header">
+                <h2>Create Product</h2>
+              </div>
+              <div className="card-body">
+                <CategoryForm />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
