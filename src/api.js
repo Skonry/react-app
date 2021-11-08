1285a67656a2170b6d5bdfd54c75f7467205bd79
skonry <skonry1604@gmail.com>
2021-11-08 22:35:25 +0100

    Moved api constants to api.js file

---
+++ src/api.js
@@ -0,0 +1,5 @@
+const organizationId = 219;
+export const apiUrl = `https://newdemostock.gopos.pl/ajax/${organizationId}`;
+export const apiHeaders = {
+  'Authorization': 'fd9ba9e1-0788-4e8f-ac46-a43df43e205e'
+};
